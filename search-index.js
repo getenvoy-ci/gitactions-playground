91a148d47250544118bf955eb11870e100d47d3b
Yaroslav Skopets <yaroslav@tetrate.io>
2020-07-24 22:06:43 +0200

    new docs

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"test_yank":{"doc":"My library v0.0.2","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["test_yank"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
