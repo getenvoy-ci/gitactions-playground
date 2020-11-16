3a942b46c4ea4cd8b1e588bf665b9dd5e4d512af
getenvoy-ci <67147606+getenvoy-ci@users.noreply.github.com>
2020-11-16 20:31:19 +0100

    `docker -ti`

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+read -ra ENVOY_DOCKER_OPTIONS <<< "${ENVOY_DOCKER_OPTIONS:-}"
+
+[[ -t 1 ]] && ENVOY_DOCKER_OPTIONS+=("-it")
+
+echo "ENVOY_DOCKER_OPTIONS=${ENVOY_DOCKER_OPTIONS}"
